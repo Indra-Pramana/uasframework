81c39bec58ec95230657296c9f0de594b6454d6f
Indra-Pramana <indrapermana01062001@gmail.com>
2022-06-30 23:51:43 -0700

    penambahan code

--- app/Controllers/Siswa.php
+++ app/Controllers/Siswa.php
@@ -14,7 +14,11 @@ class Siswa extends BaseController
         $this->siswa = new SiswaModel();
     }
 
-    
+    public function index()
+    {
+        $data['siswa'] = $this->siswa->findAll();
+        return view('siswa/index', $data);
+    }
 
     public function create()
     {
